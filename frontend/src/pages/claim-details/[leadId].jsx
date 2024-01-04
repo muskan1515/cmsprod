9aa8dcab1a1c2f0b0fbcfdfddd9b181cd4901b93
muskankushwah5 <143391688+muskankushwah5@users.noreply.github.com>
2024-01-04 12:44:03 +0530

    Update [leadId].jsx

--- frontend/src/pages/claim-details/[leadId].jsx
+++ frontend/src/pages/claim-details/[leadId].jsx
@@ -1,10 +1,8 @@
 import dynamic from "next/dynamic";
 import Seo from "../../components/common/seo";
 import ClaimDetails from "../../components/dashboard/claim-details";
-import { useRouter } from "next/router";
 
 const index = () => {
-  const router = useRouter();
   return (
     <>
       <Seo pageTitle="Claim Details" />
