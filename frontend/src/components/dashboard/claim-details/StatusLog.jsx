b8f629d343e521dcc2a765b5ec72dd67214b433f
muskankushwah5 <143391688+muskankushwah5@users.noreply.github.com>
2024-03-08 00:47:23 +0530

    Update StatusLog file for handling the status update over documents

--- frontend/src/components/dashboard/claim-details/StatusLog.jsx
+++ frontend/src/components/dashboard/claim-details/StatusLog.jsx
@@ -1,16 +1,89 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-const StatusLog = ({leadId,status,statusOptions,subStatus,claim}) => {
+const StatusLog = ({leadId,status,statusOptions,subStatus,claim,documents}) => {
 
   const [stat , setStat] = useState(0);
   const [subStage,setSubStage] = useState(0);
   const [currentStatus,setCurrentStatus]=useState(1);
 
+  console.log("status",documents);
+
+  let data = [
+    {
+      serial_num: "1",
+      doc_name: "Driving licence",
+    },
+    {
+      serial_num: "2",
+      doc_name: "Certificate of registration",
+    },
+    {
+      serial_num: "3",
+      doc_name: "Repair Estimate",
+    },
+    {
+      serial_num: "4",
+      doc_name: "Claim form",
+    },
+    {
+      serial_num: "5",
+      doc_name: "Insurance policy",
+    },
+    {
+      serial_num: "6",
+      doc_name: "Damage vehicle photographs/video",
+    },
+    {
+      serial_num: "7",
+      doc_name: "Aadhar card",
+    },
+    {
+      serial_num: "8",
+      doc_name: "Pan card",
+    },
+    {
+      serial_num: "9",
+      doc_name: " Cancel cheque",
+    },
+    {
+      serial_num: "10",
+      doc_name: " Satisfaction voucher",
+    },
+    {
+      serial_num: "11",
+      doc_name: "Discharge voucher",
+    },
+    {
+      serial_num: "12",
+      doc_name: "Dismantle photographs",
+    },
+    {
+      serial_num: "13",
+      doc_name: "Reinspection photographs",
+    },
+    {
+      serial_num: "14",
+      doc_name: "Repair Invoice",
+    },
+    {
+      serial_num: "15",
+      doc_name: "Payment/cash receipt",
+    },
+    {
+      serial_num: "16",
+      doc_name: "Images",
+    },
+    {
+      serial_num: "17",
+      doc_name: "Videos",
+    },
+  ];
+
   useEffect(()=>{
     let stat2 = {};
     if(status.length <= 0){
-
     }
     else{
 
@@ -24,17 +97,71 @@ const StatusLog = ({leadId,status,statusOptions,subStatus,claim}) => {
     setCurrentStatus(stat2?.Status);
 
   },[status.length]);
+
+  const getValueFromDocName=(docName)=>{
+    let value = -1;
+    data.map((temp,index)=>{
+      if(String(data.doc_name) === String(docName)){
+        value = temp.serial_num;
+      }
+    })
+
+    return value;
+  }
+
+  const getUpperBound = ()=>{
+    if(String(stat)=== "1")
+      return 5;
+    if(String(stat)=== "2")
+      return 8;
+    if(String(stat)=== "3")
+      return 11;
+    if(String(stat)=== "4")
+      return 14;
+    if(String(stat)=== "5")
+      return 17;
+  }
+
+
+  //function to update the status on documents validation
+  const checkIsValidated = ()=>{
+      let arr = new Array(17+1).fill(0);
+      documents?.map((doc,index)=>{
+        const value = getValueFromDocName(doc.docName);
+        arr[value]++;
+      })
+
+      const upperValue = getUpperBound();
+      console.log(upperValue);
+
+      let canUpdateStatus = true;
+      arr.map((value,index)=>{
+        if(Number(value) <= Number(upperValue) && Number(value) <= 0 ){
+            canUpdateStatus=false;
+        }
+      })
+
+      return canUpdateStatus;
+
+      
+  }
   const onSubmitHandler = ()=>{
 
+    const canUpdate = checkIsValidated();
+ 
+    if(!canUpdate && stat>1){
+      toast.error("Cannot update Status as documents are filled !");
+    }
+    else{
+    
     const userInfo = JSON.parse(localStorage.getItem("userInfo"));
     const payload = {
       LeadId : Number(leadId),
-      Status :  Number(stat) ,
+      Status :  (stat) ,
       subStage : subStage ?Number(subStage) : Number(3),
       token : Number(userInfo[0].Token)
     };
 
-    console.log(payload);
     axios.put("/api/updateStatus",payload,{
       headers:{
         Authorization:`Bearer ${userInfo[0].Token}`,
@@ -47,6 +174,7 @@ const StatusLog = ({leadId,status,statusOptions,subStatus,claim}) => {
       alert(err);
     });
   }
+  }
   const getCurrentStatus=()=>{
     let status = "";
     statusOptions.map((stat,index)=>{
@@ -55,7 +183,7 @@ const StatusLog = ({leadId,status,statusOptions,subStatus,claim}) => {
     });
     return status;
   }
-  console.log(status);
+  console.log(stat);
   return (
     <>
       <div className="col-lg-12">
@@ -69,7 +197,7 @@ const StatusLog = ({leadId,status,statusOptions,subStatus,claim}) => {
               onChange={(e)=>setStat(e.target.value)}
             >
               {statusOptions.map((stat,index)=>{
-                return ( stat.id === currentStatus+1 ||  stat.id === currentStatus) ?  <option key={index} data-tokens="Status1" value={stat.id} >{stat.value}</option>
+                return ( stat.id === currentStatus+1 || stat.id === currentStatus -1 || stat.id === currentStatus) ?  <option key={index} data-tokens="Status1" value={stat.id} >{stat.value}</option>
                   :  null
 
               })}
