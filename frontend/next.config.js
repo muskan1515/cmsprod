b27b6ddbf49196b130d34fd0aafd74478da24ae0
MTEngineerRJ <161291220+MTEngineerRJ@users.noreply.github.com>
2024-03-11 14:39:22 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
+
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: false,
   env: {
     BACKEND_DOMAIN: "https://cmsprod.onrender.com",
