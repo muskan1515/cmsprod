cfb29405795c587a48aa82e696732feb32419270
infostics <infosticstech@gmail.com>
2024-03-14 13:49:56 +0530

    changed email content

--- backend/Config/getEmailContent.js
+++ backend/Config/getEmailContent.js
@@ -10,10 +10,14 @@
         4. Claim form
         5. Insurance policy
         6. Damage vehicle photographs/video
-        7.Aadhar Card
+        7. Aadhar Card
         8. Pan Card
         9. Cancel Cheque
-        10. Satisfaction voucher
+       10. Satisfaction voucher
+       11. Previous year policy
+       12. Preinspection (if break in insurance)
+       13. NCB confirmation
+
         `
     }
     return '';
